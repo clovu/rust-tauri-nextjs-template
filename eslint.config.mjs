e4367f0d255878294bf642e52324d677bac8f88e
Clover <2621869236@qq.com>
2024-05-21 17:37:03 +0800

    chore: better format rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,26 +5,62 @@ import pluginReactConfig from "eslint-plugin-react/configs/recommended.js"
 import { fixupConfigRules } from "@eslint/compat"
 
 export default [
-  { languageOptions: { globals: { ...globals.browser, ...globals.node } } },
+  {
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+    },
+  },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   ...fixupConfigRules(pluginReactConfig),
 
   {
-    ignores: [".next/", "src-tauri/"]
+    ignores: [".next/", "src-tauri/"],
   },
 
   {
     rules: {
-      "@typescript-eslint/consistent-type-definitions": "error",
       "react/react-in-jsx-scope": "off",
-      "semi": ["error", "never"],
-      "quotes": ["error", "double"],
+      semi: ["error", "never"],
+      quotes: ["error", "double"],
+      "indent-legacy": ["error", 2],
+      "max-len": ["error", { code: 100 }],
+      "comma-dangle": ["error", "always-multiline"],
+      "comma-style": ["error", "last"],
+      "array-bracket-newline": ["error", "consistent"],
+      "object-curly-newline": ["error", { multiline: true, consistent: true }],
+      "object-curly-spacing": ["error", "always"],
+      "space-before-function-paren": ["error", {
+        anonymous: "never",
+        named: "never",
+        asyncArrow: "always",
+      }],
+      "@typescript-eslint/consistent-type-definitions": "error",
+      "@typescript-eslint/type-annotation-spacing": ["error", {
+        before: false,
+        after: true,
+        overrides: { arrow: { before: true, after: true } },
+      }],
+      "@typescript-eslint/member-delimiter-style": ["error", {
+        multiline: {
+          delimiter: "none",
+          requireLast: false,
+        },
+        singleline: {
+          delimiter: "comma",
+          requireLast: false,
+        },
+      }],
+      "space-in-parens": ["error", "never"],
+      "quote-props": ["error", "as-needed"],
       "padding-line-between-statements": [
         "error",
         { blankLine: "always", prev: "*", next: "function" },
         { blankLine: "always", prev: "function", next: "*" },
       ],
     },
-  }
+  },
 ]
