63499371bb02a669c01e6bbd072dfd62da2fa90e
Clover <2621869236@qq.com>
2024-05-21 16:24:11 +0800

    chore: use eslint in the lint-staged to fix code

---
+++ .lintstagedrc.mjs
@@ -0,0 +1,13 @@
+import path from "node:path"
+
+function createCommand(prefix, join) {
+  return (filenames) => `${prefix} ${filenames.map((f) => path.relative(process.cwd(), f)).join(` ${join} `)}`
+}
+
+export default {
+  "*.{js,jsx,ts,tsx}": [
+    createCommand("pnpm eslint --fix", ""),
+    // unlock the code to enable prettier format if you use it
+    // createCommand('prettier --write', '--write')
+  ],
+}
