5217f418dc81e5c908c18d63884b50cfd7d9c2bf
Clover <2621869236@qq.com>
2024-05-24 00:26:12 +0800

    fix: sqlite db failure after build

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,11 +1,12 @@
 #![allow(unexpected_cfgs)]
 
-use std::error;
+use std::{error, ops::Add, sync::Mutex};
 
 use serde::{Deserialize, Serialize};
 use sqlx::{migrate::MigrateDatabase, Pool, Sqlite};
+use tauri::Manager;
 
-const DB_URL: &str = "sqlite://../sqlite.db";
+static DB_URL: Mutex<String> = Mutex::new(String::new());
 
 const CREATE_TASK_SCRIPT: &str = r#"
 CREATE TABLE if NOT EXISTS task (
@@ -60,12 +61,17 @@ async fn add_task_to_db(name: String, status: i32) -> Result<(), Box<dyn error::
   Ok(())
 }
 
+/// create a sqlite connect
 async fn create_db() -> Result<Pool<Sqlite>, Box<dyn error::Error>> {
-  if !sqlx::Sqlite::database_exists(DB_URL).await.unwrap_or(false) {
-    sqlx::Sqlite::create_database(DB_URL).await?;
+  let url = DB_URL.lock().unwrap().clone();
+  let url = url.as_str();
+
+  println!("db url: {}", url);
+  if !sqlx::Sqlite::database_exists(url).await.unwrap_or(false) {
+    sqlx::Sqlite::create_database(url).await?;
   }
 
-  let db = sqlx::SqlitePool::connect(DB_URL).await?;
+  let db = sqlx::SqlitePool::connect(url).await?;
 
   sqlx::query(CREATE_TASK_SCRIPT).execute(&db).await?;
 
@@ -84,6 +90,17 @@ async fn get_task_list() -> Result<Vec<Task>, Box<dyn error::Error>> {
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub async fn run() {
   tauri::Builder::default()
+    .setup(|app| {
+      let app_cache_dir = app.path().app_cache_dir().unwrap();
+      let app_cache_dir = app_cache_dir.to_str().unwrap();
+      let app_cache_dir = String::from("sqlite:").add(app_cache_dir).add("/cache.db");
+
+      println!("DB_URL ==>> {}", app_cache_dir);
+
+      *DB_URL.lock().unwrap() = app_cache_dir;
+
+      Ok(())
+    })
     .plugin(tauri_plugin_shell::init())
     .invoke_handler(tauri::generate_handler![get_tasks, add_task])
     .run(tauri::generate_context!())
