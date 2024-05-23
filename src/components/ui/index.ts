c8c9fa6d461c3843f7ed1f0b08c146d12fbba7cf
Clover <2621869236@qq.com>
2024-05-23 11:21:21 +0800

    feat: export component in index

---
+++ src/components/ui/index.ts
@@ -0,0 +1,7 @@
+export { Button } from "./button"
+export type { ButtonProps } from "./button"
+
+export { Input } from "./input"
+export type { InputProps } from "./input"
+
+export * from "./card"
