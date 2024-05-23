bc50b5dce2c83dc7073a69adc01432836bfa1e8d
Clover <2621869236@qq.com>
2024-05-23 09:50:14 +0800

    chore: lint config

--- .lintstagedrc.mjs
+++ .lintstagedrc.mjs
@@ -2,7 +2,7 @@ import path from "node:path"
 
 function createCommand(prefix, join) {
   return (filenames) =>
-    `${prefix} ${filenames.map((f) => path.relative(process.cwd(), f)).join(` ${join} `)}`
+    `${prefix} ${filenames.map((f) => path.relative(process.cwd(), f)).join(`${join} `)}`
 }
 
 export default {
